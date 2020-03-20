555b5f02954df3ed70dac7e226443687e7f9a39d
mike <Tsh85641399>
2020-03-20 15:58:55 +0800

    修改界面

--- src/main/java/chat/ChatEventDefine.java
+++ src/main/java/chat/ChatEventDefine.java
@@ -1,6 +1,11 @@
 package chat;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ChatEventDefine {
+
+    private static final Logger logger = LoggerFactory.getLogger(ChatEventDefine.class);
 
     private ChatInit chatInit;
     private IChatEvent chatEvent;
@@ -13,6 +18,8 @@ public class ChatEventDefine {
         min();
         quit();
         chatInit.move();
+        barChat();
+        barFriend();
     }
 
     private void min(){
@@ -35,5 +42,49 @@ public class ChatEventDefine {
             chatInit.close();
             System.exit(0);
         });
+    }
+
+
+    public void barFriend(){
+        chatInit.bar_friend.setOnAction(event -> {
+            switchBarChat(false);
+            switchBarFriend(true);
+        });
+    }
+
+    public void barChat(){
+        chatInit.bar_chat.setOnAction(event -> {
+            switchBarFriend(false);
+            switchBarChat(true);
+        });
+    }
+
+
+    private void switchBarChat(boolean toggle){
+        if (toggle){
+            // 显示聊天窗口，变化图标
+            chatInit.group_bar_chat.setVisible(true);
+            chatInit.bar_chat.setStyle("-fx-background-image: url('/chat/img/system/chat_2.png')");
+            logger.info("显示聊天窗口");
+        }else{
+            // 关闭聊天窗口，变化图标
+            chatInit.group_bar_chat.setVisible(false);
+            chatInit.bar_chat.setStyle("-fx-background-image: url('/chat/img/system/chat_0.png')");
+            logger.info("关闭聊天窗口");
+        }
+    }
+
+    private void switchBarFriend(boolean toggle){
+        if (toggle){
+            // 显示好友窗口，变化图标
+            chatInit.group_bar_friend.setVisible(true);
+            chatInit.bar_friend.setStyle("-fx-background-image: url('/chat/img/system/friend_2.png')");
+            logger.info("显示好友窗口");
+        }else{
+            // 关闭好友窗口，变化图标
+            chatInit.group_bar_friend.setVisible(false);
+            chatInit.bar_friend.setStyle("-fx-background-image: url('/chat/img/system/friend_0.png')");
+            logger.info("关闭好友窗口");
+        }
     }
 }
--- src/main/java/chat/ChatInit.java
+++ src/main/java/chat/ChatInit.java
@@ -5,6 +5,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.stage.StageStyle;
 import view.UIObject;
@@ -21,11 +22,16 @@ public abstract class ChatInit extends UIObject {
 
     public IChatEvent chatEvent;
 
-    public TextArea txt_input;
-    public Button group_bar_chat_min;
-    public Button group_bar_friend_min;
-    public Button group_bar_chat_close;
-    public Button group_bar_friend_close;
+    public TextArea txt_input;//输入文字框
+    public Button group_bar_chat_min;//聊天窗口最小化
+    public Button group_bar_friend_min;//好友窗口最小化
+    public Button group_bar_chat_close;//聊天窗口关闭
+    public Button group_bar_friend_close;//好友窗口关闭
+    public Button bar_chat;//聊天窗口按钮
+    public Button bar_friend;//好友窗口按钮
+    public Pane group_bar_chat; //聊天窗口
+    public Pane group_bar_friend; //好友窗口
+
 
 
     public ChatInit(IChatEvent chatEvent) {
@@ -51,6 +57,10 @@ public abstract class ChatInit extends UIObject {
         group_bar_friend_min = $("group_bar_friend_min",Button.class);
         group_bar_chat_close = $("group_bar_chat_close",Button.class);
         group_bar_friend_close = $("group_bar_friend_close",Button.class);
+        bar_chat = $("bar_chat", Button.class);
+        bar_friend = $("bar_friend", Button.class);
+        group_bar_chat = $("group_bar_chat", Pane.class);
+        group_bar_friend = $("group_bar_friend", Pane.class);
     }
 
     public Parent root(){
